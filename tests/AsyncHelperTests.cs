aecd044c961372a03e84c90eb7519e4955883fe7
Tarjei Olsen <tarjei.olsen@epinova.no>
2019-07-23 12:52:16 +0200

    Add async helper implementation

---
+++ tests/AsyncHelperTests.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Epinova.Infrastructure;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Epinova.InfrastructureTests
+{
+    public class AsyncHelperTests
+    {
+        private readonly ITestOutputHelper _output;
+
+        public AsyncHelperTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void RunSync_TaskWithReturnValue_Works()
+        {
+            string result = AsyncHelper.RunSync(() => StringFetcherTask("hello, there"));
+
+            _output.WriteLine("Result: " + result);
+
+            Assert.Equal("You said 'hello, there'", result);
+        }
+
+        [Fact]
+        public void RunSync_VoidTask_Works()
+        {
+            var input = new[] { "hello, there" };
+
+            AsyncHelper.RunSync(() => VoidTask(input));
+
+            _output.WriteLine("Result: " + input);
+
+            Assert.Equal("Modified: 'hello, there'", input.First());
+        }
+
+        private static async Task<string> StringFetcherTask(string input)
+        {
+            return await Task.FromResult($"You said '{input}'");
+        }
+
+        private static async Task VoidTask(IList<string> input)
+        {
+            await Task.Run(() => { input[0] = $"Modified: '{input[0]}'"; });
+        }
+    }
+}
